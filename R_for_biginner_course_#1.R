ad22100bc4704cfcf7a3b5ff004e85ce01b02422
Ng'winamila Kasongi <36668552+nkasongi@users.noreply.github.com>
2024-02-07 03:00:08 +0300

    Add files via upload

---
+++ R_for_biginner_course_#1.R
@@ -0,0 +1,48 @@
+## create R data objects
+
+# vector object
+# we use c() to create a vector object
+Names = c('John', 'Daniel', 'Lisa')
+Age = c(28,50,70)
+
+# list object
+# we use list() to create a list object
+list1 = list('John',28, 'Daniel', 50, 'Lisa', 70)
+list1
+
+# matrix object
+# we use matrix() to create a matrix object
+matrix1 = matrix(1:4,nrow=2,ncol=2)
+matrix2 = matrix(6:9,nrow=2,ncol=2)
+
+# Arrays object
+# we use array() to create a data.frame object
+array1 = array(c(matrix1,matrix2),dim = c(2,2,2)) # two-dimensional array
+array1
+
+# data frame
+# we use data.frame() to create a data.frame object
+table1 = data.frame(Names=c('John', 'Daniel', 'Lisa'),Age=c(28,50,70))
+table1
+
+# factor
+# we use factor() to factor object in R
+
+
+
+#------------------------------End-----------------------------------------
+
+# if statement
+
+# We shall create a if statement that print hot day if the temperature above 30
+
+temperature = 35
+
+if(temperature > 30) {print('hot day')}
+
+if(temperature > 40){print(print('hot day'))} else{print('cool day')}
+
+
+#-----------------------------End------------------------------------------
+
+# for loop
